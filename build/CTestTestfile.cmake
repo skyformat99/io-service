9a705fac361694961198e720f058581ac8497990
savagecm <savagecm@qq.com>
2017-08-27 09:51:51 +0800

    first commit

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/max/study/github/io-service
-# Build directory: /home/max/study/github/io-service/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("example")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/max/study/github/io-service/src
-# Build directory: /home/max/study/github/io-service/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
